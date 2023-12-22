cfdb59414254c139514a1adb3483e851f581f4d2
Mjasim <mjasimmc@gmail.com>
2023-12-22 14:05:41 +0530

    some space along sides

--- src/components/ImageThree.jsx
+++ src/components/ImageThree.jsx
@@ -9,18 +9,19 @@ const ImageThree = () => {
     const {fullPageStrength} = useContext(StrengthContext)
    
     return (
-        <Parallax bgImage={Image} strength={fullPageStrength.use?fullPageStrength.value:strength} blur={{ min: -15, max: 15 }}>
+        <Parallax bgImage={Image} className='flex items-center justify-center bg-no-repeat md:m-[4rem]' strength={fullPageStrength.use?fullPageStrength.value:strength} >
             <div className={`w-full min-h-screen flex flex-col justify-center items-center  `} >
                 <h1 className='heading text-white md:text-3xl font-bold'>Welcome To Image Slider With Parallax Effect</h1>
                 <div className="w-[20rem] grid gap-2 bg-[#c9c9c9] p-2 rounded-md">
-                <div className="flex gap-2 items-center">
+                    <div className="flex gap-2 items-center">
                         <p>Strength</p> <input type="number" className='p-2 px-5 outline-none rounded-md flex-1 caret-black' value={strength}
                             onChange={(e) => setStrength(e.target.value)}
                         />
                     </div>
+                   
                 </div>
             </div>
-        </Parallax>)
+        </Parallax>);
 }
 
 export default ImageThree;
